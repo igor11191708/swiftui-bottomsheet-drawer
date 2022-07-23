660051450589ab7f111265ba3dc824f45863e7e5
Igor <igorshelopaev@gmail.com>
2022-07-23 10:19:05 +0300

    code format

--- Sources/swiftui-bottom-sheet-drawer/key/BottomSheetPosisionKey.swift
+++ Sources/swiftui-bottom-sheet-drawer/key/BottomSheetPosisionKey.swift
@@ -10,6 +10,7 @@ import SwiftUI
 /// Key for emerging drawer sheet position
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 6.0, *)
 public struct BottomSheetPosisionKey: PreferenceKey {
+    
     static public var defaultValue: BottomSheetPosision = .down(0)
 
     static public func reduce(value: inout BottomSheetPosision, nextValue: () -> BottomSheetPosision) {
--- Sources/swiftui-bottom-sheet-drawer/key/SizePreferenceKey.swift
+++ Sources/swiftui-bottom-sheet-drawer/key/SizePreferenceKey.swift
@@ -10,7 +10,9 @@ import SwiftUI
 /// Key for emerging size
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 6.0, *)
 public struct SizePreferenceKey: PreferenceKey {
+    
     public static var defaultValue = CGSize(width: 0, height: 0)
+    
     public static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
         value = nextValue()
     }
